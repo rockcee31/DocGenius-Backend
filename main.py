a655ebfbe6c3133f9a61f6d9463f1588e25cb6b2
rockcee31 <thapliyalrohit31@gmail.com>
2025-06-06 09:37:29 +0530

    Initial commit (clean, no secrets)

---
+++ main.py
@@ -0,0 +1,118 @@
+from fastapi import FastAPI,UploadFile,File,Request
+import uvicorn
+from dotenv import load_dotenv
+import uuid
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from openai import OpenAI
+import os
+def main():
+    print("Hello from backend!")
+    uvicorn.run(app,host="0.0.0.0",port="8000")
+
+app = FastAPI()
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+@app.post("/upload")
+async def upload_endpoint(file: UploadFile = File(...)):
+    temp_filename = f"temp_{uuid.uuid4().hex}.pdf"
+    with open(temp_filename, "wb") as f:  #open the file in binary write mode(wb) With is used for automatically closing file after write
+        contents = await file.read()
+        f.write(contents)
+
+     # Use LangChain's PyPDFLoader to load the PDF
+    loader = PyPDFLoader(file_path=temp_filename)
+    docs = loader.load()
+
+   
+    # Chunking
+    text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=400
+    )
+    split_docs = text_splitter.split_documents(documents=docs)
+
+    # Vector Embeddings
+    embedding_model = OpenAIEmbeddings(
+    model="text-embedding-3-large"
+    )
+
+    # Using [embedding_model] create embeddings of [split_docs] and store in DB
+
+    vector_store = QdrantVectorStore.from_documents(
+        documents=split_docs,
+        url="http://localhost:6333",
+        collection_name="learning_vectors",
+        embedding=embedding_model
+)   
+     # Clean up (optional but good practice)
+    os.remove(temp_filename)
+
+    if vector_store:
+        return {"status": "uploaded"}
+
+
+@app.post("/chat")
+async def chat(request: Request):
+    data = await request.json()
+    messages = data.get("messages")
+
+   
+    # Vector Embeddings
+    embedding_model = OpenAIEmbeddings(
+        model="text-embedding-3-large"
+        )       
+
+    vector_db = QdrantVectorStore.from_existing_collection(
+        url="http://localhost:6333",
+        collection_name="learning_vectors",
+        embedding=embedding_model
+        )
+
+    # Take User Query
+    query =  messages[-1].get('content')
+
+    # Vector Similarity Search [query] in DB
+    search_results = vector_db.similarity_search(
+    query=query
+        )
+
+    context = "\n\n\n".join([f" PageContent:{result.page_content}\n Page Number: {result.metadata['page_label']} \nFile Location: {result.metadata['source']}"  for result in search_results])
+
+    SYSTEM_PROMPT = f"""
+            you are an helpful Ai Assistant who answers user query based on the available context retrieved from 
+            PDF file along with page content and and pageNumber
+
+            you should only answer user based on the following context and navigate the user to open the right page number to know more
+
+            Context:
+            {context}
+            """
+
+    messages.insert(0, {"role": "system", "content": SYSTEM_PROMPT})
+
+    chat_completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    return {"answer": chat_completion.choices[0].message.content}
+
+
+
+
+
+@app.get('/')
+async def root():
+    return {"message":"Hello chai code"}
+
+if __name__ == "__main__":
+    main()
+
+
+# 
